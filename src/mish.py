7564fe88b581207aef1b19e70e3aded0eb16db59
sarah-antillia <sarah@antillia.com>
2024-09-01 16:09:58 +0900

    Fixed a bug in src/mish.py

--- src/mish.py
+++ src/mish.py
@@ -7,5 +7,5 @@ import tensorflow as tf
 def mish(x):
     # 2024/09/01 commnet out the following line
     #x = tf.convert_to_tensor(x) #Added this line
-    print(">>> mish---")
+    #print(">>> mish---")
     return tf.math.multiply(x, tf.math.tanh(tf.math.softplus(x)))
